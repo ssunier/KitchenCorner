bbe6a11c34062a5373108ecc93926c0a1f89945b
Sheridan Sunier <ssunier@wellesley.edu>
2014-04-21 20:07:37 -0400

    Update tables.sql

--- tables.sql
+++ tables.sql
@@ -55,6 +55,13 @@ create table recipequantity (
        )
        ENGINE = InnoDB;
 
+
+INSERT into users VALUES (1, 'skim22', 'abc', 'Soojin'),
+                         (2, 'ssunier','random', 'Sheridan'),
+                         (3, 'test','lalala','TestUser');
+
+INSERT into fridge (uid) VALUES (1),(2),(3);
+
 INSERT into ingredient VALUES (1, 'flour', 'cup'),
                               (2, 'cheese', 'cup'),
                               (3, 'broccoli', 'head'),
@@ -69,8 +76,6 @@ INSERT into ingredient VALUES (1, 'flour', 'cup'),
                               (12,'ground nutmeg','tsp'),
                               (13, 'ground cinnamon','tsp');
 
-INSERT into fridge (uid) VALUES (1),(2),(3);
-
 INSERT into fridgequantity VALUES
                               (1, 1, 5),
                               (3, 2, 1),
@@ -82,9 +87,9 @@ INSERT into fridgequantity VALUES
                               (2, 9, 1);
 
 INSERT into recipe VALUES
-      (3, 'Italian Pasta Salad',20,null,1,
+      (1, 'Italian Pasta Salad',20,null,1,
             'In large bowl, toss all ingredients together. Add dressing last and mix well.'),
-      (3, 'Pumpkin Pudding',10,null,2,
+      (2, 'Pumpkin Pudding',10,null,2,
             '1. In a bowl, combine pumpkin, molasses and spices. Gradually add milk. Add pudding mix; beat slowly with an electric mixer until thick, about 1 minute. 
              2. Fold in whipped cream.
              3. Pour into a serving bowl or individual serving dishes. 
@@ -101,3 +106,4 @@ INSERT into recipequantity VALUES
                               (2,11,1),
                               (2,12,.25),
                               (2,13,.25);
+                              
