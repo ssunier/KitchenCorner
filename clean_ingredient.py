c9e94619f58b669be3d73e642ebbf65507f5ae93
Sheridan Sunier <ssunier@wellesley.edu>
2014-04-23 20:04:20 -0400

    Update clean_ingredient.py

--- clean_ingredient.py
+++ clean_ingredient.py
@@ -21,86 +21,90 @@ from collections import defaultdict, Counter
 measurements = [w.strip() for w in open('measurements.txt').readlines()]
 meaSet = set(measurements)
 
+# TESTING
+"""
+TIMEOUT = 10.0
+RETRIES = 0
+client = Client(api_id='d9efc743', api_key='bbb0c1439402e1181312a67278a37342', timeout=TIMEOUT, retries=RETRIES)
+search = client.search("curry")
+match = search.matches[0]	
+recipe = client.recipe(match.id)
+"""
+
 """
 	Given a recipe, single out the quantities
 	and separate from the rest of the ingredient string
 """
-def split(recipe):
-	i = defaultdict(lambda : defaultdict())
 
-	p = '^((\d+( \d+/\d+)?)|(\d+/\d+))( (.+))?$'
+class Clean(object):
 
- 	# for some reason when I make verbose it stops working... idk why
- 	"""
-		^				# beginning of string
-		((\d+			# digit in range 0-9 1 or more times
-			( \d+/		# matches any decimal digit in the second group
-				\d+)	# matches any decimal digit in the thrid group (none)
-				?)		# match expression in capture
-		|				# or
-		(\d+/			# more decimal matching! (should be none)
-			\d+))		# more decimal matching in this group (could be none)
-		( (.+))			# the remaining string (group 5)
-		?				# creates groups
-		$				# end of string
+	def __init__(self):
+		self.recipe_ingred = defaultdict(lambda : defaultdict())
+
+	def split(self, recipe):
+		i = defaultdict(lambda : defaultdict())
+
+		p = '^((\d+( \d+/\d+)?)|(\d+/\d+))( (.+))?$'
+
+ 		# for some reason when I make verbose it stops working... idk why
+ 		"""
+			^				# beginning of string
+			((\d+			# digit in range 0-9 1 or more times
+				( \d+/		# matches any decimal digit in the second group
+					\d+)	# matches any decimal digit in the thrid group (none)
+					?)		# match expression in capture
+			|				# or
+			(\d+/			# more decimal matching! (should be none)
+				\d+))		# more decimal matching in this group (could be none)
+			( (.+))			# the remaining string (group 5)
+			?				# creates groups
+			$				# end of string
+		"""
+		for ingred in recipe.ingredientLines:
+			m = re.match(p, ingred)
+			try:
+				x = self.getMeasurement(m.group(5))
+				if x is None:
+					self.recipe_ingred[m.group(5)][m.group(1)] = ""
+				else:
+					cleaned = self.removeMeasurement(m.group(5),x)
+					self.recipe_ingred[cleaned][m.group(1)] = x
+			
+			except:
+				print "exception"
+		print self.recipe_ingred
+		return self.recipe_ingred
+
+
 	"""
+		Given the remaining string, separate the measurement if it is in the set
+		of measurements declared at the top of this file.  If there is no matching
+		measurement, then set the unit to be none.
+	"""
+	def getMeasurement(self, group):
+		value = None
+		for k in re.findall('\w+', group):
+			if k in meaSet:
+				value = re.sub("s$", "",k)
+				#print meaSet
+		return value
 
-	for ingred in recipe.ingredientLines:
-		m = re.match(p, ingred)
-		
-		x = getMeasurement(m.group(5))
+	def removeMeasurement(self, group, measurement):
+		clean = ""
+		for k in re.findall('\w+', group):
+			if k in meaSet:
+				continue
+			clean += k + " "
+		return clean
 
-		if x is None:
-			i[m.group(5)][m.group(1)] = ""
-
-		else:
-			cleaned = removeMeasurement(m.group(5),x)
-
-			i[cleaned][m.group(1)] = x
-
-	print i
-	return i
-
-
-"""
-	Given the remaining string, separate the measurement if it is in the set
-	of measurements declared at the top of this file.  If there is no matching
-	measurement, then set the unit to be none.
-"""
-def getMeasurement(group):
-	value = None
-	for k in re.findall('\w+', group):
-		if k in meaSet:
-			value = re.sub("s$", "",k)
-			break
-	return value
-
-def removeMeasurement(group, measurement):
-	clean = ""
-	for k in re.findall('\w+', group):
-		if k in meaSet:
-			continue
-		clean += k + " "
-	return clean
-
-
-"""
-Testing Data - Comment back out when done
-"""
-TIMEOUT = 5.0
-RETRIES = 0
-
-client = Client(api_id=API_ID, api_key=API_KEY, timeout=TIMEOUT, retries=RETRIES)
-
-search = client.search('red curry paste')
-match = search.matches[0]
-recipe = client.recipe(match.id)
-
-#print recipe
+	#def main(recipe):
+	#	try:
+	#		split(recipe)
+	#	except:
+	#		print "something happened"
 
 
 if __name__ == '__main__':
-	split(recipe)
-
-
+	c = Clean()
+	c.split(recipe)
 
